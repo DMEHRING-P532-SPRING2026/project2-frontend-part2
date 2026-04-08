10d778c7aee527c042daae429b0be2306c96574f
DevinMehringer <devin.mehringer@gmail.com>
2026-04-07 21:25:54 -0400

    redirect issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Fufillment from './pages/Fufillment'
 import Order from './pages/Order'
 import Admin from './pages/Admin'
@@ -6,6 +6,7 @@ import Admin from './pages/Admin'
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/fufillment/lab" replace />} />
       <Route path="/fufillment/lab" element={<Fufillment type="LAB"/>} />
       <Route path="/fufillment/medication" element={<Fufillment type="MEDICATION"/>} />
       <Route path="/fufillment/imaging" element={<Fufillment type="IMAGING"/>} />

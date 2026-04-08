693cac4c0d01a326554a998a5f35da7a25a249cc
DevinMehringer <devin.mehringer@gmail.com>
2026-04-07 21:31:56 -0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
-import Fufillment from './pages/Fufillment'
+import Fufillment from './pages/Fulfillment'
 import Order from './pages/Order'
 import Admin from './pages/Admin'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/fufillment/lab" replace />} />
-      <Route path="/fufillment/lab" element={<Fufillment type="LAB"/>} />
-      <Route path="/fufillment/medication" element={<Fufillment type="MEDICATION"/>} />
-      <Route path="/fufillment/imaging" element={<Fufillment type="IMAGING"/>} />
+      <Route path="/" element={<Navigate to="/fulfillment/lab" replace />} />
+      <Route path="/fulfillment/lab" element={<Fufillment type="LAB"/>} />
+      <Route path="/fulfillment/medication" element={<Fufillment type="MEDICATION"/>} />
+      <Route path="/fulfillment/imaging" element={<Fufillment type="IMAGING"/>} />
       <Route path="/order" element={<Order />}/>
       <Route path="/admin" element={<Admin />}/>
     </Routes>
